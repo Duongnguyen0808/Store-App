40f6e40e4704b4953beada2c8db35a596804a341
lecongdatd36 <lecongdatd36@gmail.com>
2025-04-27 15:02:43 +0700

    cap nhat auth id

--- backend_api/routes/auth.js
+++ backend_api/routes/auth.js
@@ -51,5 +51,28 @@ authRouter.post('/api/signin', async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 })
+authRouter.put('/api/users/:id', async (req, res) => {
+    try {
+        //Extract the 'id' parameter from the request URL
+        const { id } = req.params;
+
+        //Extract the "state", "city" and locality fields from the request body
+        const { state, city, locality } = req.body;
+
+        //Find the user by their ID and update the state, city and locality
+        // the {new:true} option ensures the updated document is returned
+        const updatedUser = await User.findByIdAndUpdate(
+            id,
+            { state, city, locality },
+            { new: true },
+        );
+        if (!updatedUser) {
+            return res.status(404).json({ error: "User not found" });
+        }
+        return res.status(200).json(updatedUser);
+    } catch (error) {
+        res.status(500).json({ message: "Failed to update user." });
+    }
+});
 
 module.exports = authRouter;
